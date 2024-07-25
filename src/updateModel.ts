836ab2617b3885ad0891acf35d04d118bcf0f729
smonist <s.wesp@gmx.net>
2024-07-25 19:03:50 +0200

    a

--- src/updateModel.ts
+++ src/updateModel.ts
@@ -1,7 +1,7 @@
 import { readFileSync } from "node:fs";
 import { Ollama } from "ollama";
 
-const modelfile = readFileSync("Modelfile", "utf-8");
+const modelfile = readFileSync("Modelfile-Messages", "utf-8");
 
 const ollama = new Ollama({ host: "http://192.168.0.28:11434" });
 await ollama.create({
