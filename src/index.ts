aea64b2eb2e2efac86d9853b0156613899473340
Yannic <me@yannic.at>
2024-07-25 18:02:17 +0200

    commit

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,12 @@
-import { Ollama } from 'ollama'
+import { Ollama } from "ollama";
 
-const ollama = new Ollama({ host: 'http://127.0.0.1:11434' })
-const message = { role: 'user', content: 'Why is the sky blue?' }
-const response = await ollama.chat({ model: 'llama3.1:8b', messages: [message], stream: true })
+const ollama = new Ollama({ host: "http://127.0.0.1:11434" });
+const message = { role: "user", content: "Wer bist du" };
+const response = await ollama.chat({
+	model: "llama3.1:8b",
+	messages: [message],
+	stream: true,
+});
 for await (const part of response) {
-  process.stdout.write(part.message.content)
+	process.stdout.write(part.message.content);
 }
